d599fb81be4e895d1a8438d4577d409d36deb943
Jose Ramon Bogarin Valenzuela <tec.jrbogarin@gmail.com>
2023-10-25 19:46:56 -0700

    agregar configuracion tema

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.deepOrange,
+  Colors.pink,
+  Colors.pinkAccent
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0 && selectedColor < colorList.length ,
+            'La selección de color no cumple con el rango de 0 asta ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor]
+  );
+}
