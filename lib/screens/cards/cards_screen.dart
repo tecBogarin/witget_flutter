371e88a67a88a44ecd6b33474aa7d8f9da09e6c5
Jose Ramon Bogarin Valenzuela <tec.jrbogarin@gmail.com>
2023-10-29 01:18:38 -0700

    creando otro tipo de tarjetas

--- lib/screens/cards/cards_screen.dart
+++ lib/screens/cards/cards_screen.dart
@@ -25,7 +25,7 @@ class CardsScreen extends StatelessWidget {
             ...cards.map((card) =>
                 _CardType1(label: card['label'], elevation: card['elevation'])),
             ...cards.map((card) =>
-                _CardType1(label: card['label'], elevation: card['elevation'])),
+                _CardType2(label: card['label'], elevation: card['elevation'])),
           ],
         ),
       ),
@@ -58,4 +58,40 @@ class _CardType1 extends StatelessWidget {
           ),
         ),
       );
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors =Theme.of(context).colorScheme;
+    return Card(
+    
+        elevation: elevation,
+        shape:  RoundedRectangleBorder(
+          side: BorderSide(
+            color: colors.outline
+          ),
+            borderRadius: const BorderRadius.all(Radius.circular(15))),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.more_vert_outlined))),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(label),
+              )
+            ],
+          ),
+        ),
+      );
+      }
 }
