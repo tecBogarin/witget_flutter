b13f02ad47239db2791a36a9999c27b803026f21
Jose Ramon Bogarin Valenzuela <tec.jrbogarin@gmail.com>
2023-10-29 01:07:30 -0700

    creando tarjetas

--- lib/screens/cards/cards_screen.dart
+++ lib/screens/cards/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
+];
+
 class CardsScreen extends StatelessWidget {
   static const String screenName = 'card_screen';
   const CardsScreen({super.key});
@@ -10,7 +19,43 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cards Screen'),
       ),
-      body: const Placeholder(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ...cards.map((card) =>
+                _CardType1(label: card['label'], elevation: card['elevation'])),
+            ...cards.map((card) =>
+                _CardType1(label: card['label'], elevation: card['elevation'])),
+          ],
+        ),
+      ),
     );
   }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) => Card(
+        elevation: elevation,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.more_vert_outlined))),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(label),
+              )
+            ],
+          ),
+        ),
+      );
 }
