dac35d351fefac373e5a345734fec0f32bc7db64
Jose Ramon Bogarin Valenzuela <tec.jrbogarin@gmail.com>
2023-10-30 21:23:29 -0700

    cambios

---
+++ lib/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollScreen extends StatelessWidget {
+  static const name = 'tutorial_screen';
+  const InfiniteScrollScreen({super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,3 +1,4 @@
 export 'package:widgets_app/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/screens/cards/cards_screen.dart';
 export 'package:widgets_app/screens/home/home_screen.dart';
+export 'package:widgets_app/screens/animated/animated_screen.dart';
