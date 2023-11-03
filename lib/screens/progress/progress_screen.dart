f184d0801492b8d003023a99f2d7fbb71b5b2b15
Jose Ramon Bogarin Valenzuela <tec.jrbogarin@gmail.com>
2023-11-02 20:13:24 -0700

    progress indicator

--- lib/screens/progress/progress_screen.dart
+++ lib/screens/progress/progress_screen.dart
@@ -6,6 +6,41 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Progress Indicators'),
+        ),
+        body: const _ProgressView());
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          Text('Indicador de progreso circular ',style: TextStyle(fontSize: 16)),
+          SizedBox(height: 20),
+          CircularProgressIndicator( strokeWidth: 2,backgroundColor: Colors.black45),
+           SizedBox(height: 30),
+          Text('Indicador de progreso controlado ',style: TextStyle(fontSize: 16)),
+          SizedBox(height: 20),
+          _ControllersProgressIndicator()
+        ],
+      )
+    );
+  }
+}
+
+class _ControllersProgressIndicator extends StatelessWidget {
+  const _ControllersProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircularProgressIndicator( strokeWidth: 2,backgroundColor: Colors.black45);
   }
 }
